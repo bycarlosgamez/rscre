20beeb45cc6c4ae733d226a369dd2ec6186bb736
Carlos Gámez <hardpx@gmail.com>
2023-12-15 12:10:45 -0600

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,13 @@
-import { useId, useState } from 'react';
 import colors from './assets/colors';
 import ColorBox from './components/ColorBox/ColorBox';
 
 import './App.css';
 
 function App() {
-  const id = useId;
-  const random = (arr) => {
-    const random = Math.floor(Math.random() * arr.length);
-    return arr[random];
-  };
-  const [color, setColor] = useState(random(colors));
-
-  const handleClick = (id) => {
-    boxes.map((box, i) => {
-      return box[i] === id ? setColor(random(colors)) : box;
-    });
-  };
-
   const boxes = [];
-  for (let i = 0; i < 26; i++) {
-    boxes.push(<ColorBox color={color} handleClick={handleClick} key={i} />);
+
+  for (let i = 0; i < 25; i++) {
+    boxes.push(<ColorBox colors={colors} key={i} />);
   }
 
   return <div className='BoxesGrid'>{boxes}</div>;
--- src/components/ColorBox/ColorBox.jsx
+++ src/components/ColorBox/ColorBox.jsx
@@ -1,7 +1,19 @@
 import { useState } from 'react';
 import './ColorBox.css';
 
-export default function ColorBox({ color, handleClick }) {
+export default function ColorBox({ colors }) {
+  const random = (arr) => {
+    const random = Math.floor(Math.random() * arr.length);
+    return arr[random];
+  };
+
+  const [color, setColor] = useState(random(colors));
+
+  const handleClick = () => {
+    const color = random(colors);
+    setColor(color);
+  };
+
   return (
     <div
       className='colorBox'
